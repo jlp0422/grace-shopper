f235975736ceefe7ddd496a7fe16d23b88881046
Alice Luong <aliceluong@Alices-MacBook-Air.local>
2018-04-19 19:28:08 -0400

    routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,2 +1,34 @@
 const app = require('express').Router()
 module.exports = app;
+
+app.get('/', (req, res, next) => {
+    User.findAll()
+        then(users => res.send(users))
+        .catch(next);
+})
+
+app.post('/', (req, res, next) => {
+    User.create(req.body)
+        .then(user => res.send(user))
+        .catch(next);
+})
+
+app.put('/:id', (req, res, next) => {
+    User.findById(req.params.id)
+        .then(user => {
+            Object.assign(user, req.body)
+            return user.save()
+        })
+        .then(user => res.send(user))
+        .catch(next);
+})
+
+app.delete('/:id', (req, res, next) => {
+    User.findById(req.params.id)
+        .then(user => {
+            return user.destroy()
+        })
+        .then(() => res.sendStatus(204))
+        .catch(next);
+})
+
