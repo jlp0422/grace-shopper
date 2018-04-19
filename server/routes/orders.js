9d6571f3b190a60adde0ad8c1e7f43493de03801
Alice Luong <aliceluong@Alices-MacBook-Air.local>
2018-04-19 19:57:18 -0400

    routes new

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,2 +1,33 @@
 const app = require('express').Router()
 module.exports = app;
+
+app.get('/', (req, res, next) => {
+    Order.findAll()
+        .then(orders => res.send(orders))
+        .catch(next);
+})
+
+app.post('/', (req, res, next) => {
+    Order.create(req.body)
+        .then(order => res.send(order))
+        .catch(next);
+})
+
+app.put('/:id', (req, res, next) => {
+    Order.findById(req.params.id)
+        .then(order => {
+            Object.assign(order, req.body)
+            return order.save();
+        })
+        .then(order => res.send(order))
+        .catch(next);
+})
+
+app.delete('/:id', (req, res, next) => {
+    Order.findById(req.params.id)
+        .then(order => {
+            return order.destroy()
+        })
+        .then (() => res.sendStatus(204))
+        .catch(next);
+})
