3d39a1a3e88c103bd39ebe2f5ab2c58decafa05d
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-04-21 18:31:08 -0400

    past orders works

--- client/src/components/Order/OrderCard.js
+++ client/src/components/Order/OrderCard.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const OrderCard = ({ orderItems, products }) => {
+const OrderCard = ({ orderItems, order }) => {
   return (
-    <div>
+    <div style={{backgroundColor: '#f0f3f8'}}>
+      <h4>Order date: {order.date}</h4>
       {
         orderItems.map(item => (
-          <h5 key={item.id}>{item.quantity}</h5>
+          <div key={item.id}>
+            <h4>{item.product.name}</h4>
+            <h5>Quantity: {item.quantity}</h5>
+            <h5>Price: ${item.product.price}</h5>
+          </div>
         ))
       }
     </div>
@@ -16,7 +21,7 @@ const OrderCard = ({ orderItems, products }) => {
 const mapState = ({ lineItems }, { order }) => {
   const orderItems = lineItems.filter(item => item.orderId === order.id)
   console.log(orderItems)
-  return { orderItems }
+  return { orderItems, order }
 }
 
 export default connect(mapState)(OrderCard)
