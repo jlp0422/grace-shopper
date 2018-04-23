78cc007990c9afbf5c60c03a26fc6eb086e39ba3
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-04-22 21:01:49 -0400

    fixed route casing

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const app = require('express').Router();
 module.exports = app;
 
 app.use('/categories', require('./categories'));
-app.use('/lineitems', require('./lineitems'));
+app.use('/lineitems', require('./lineItems'));
 app.use('/orders', require('./orders'));
 app.use('/products', require('./products'));
 app.use('/users', require('./users'));
