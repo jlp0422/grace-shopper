722ace925ae42fe2ee2f8bcbe0d745d0a00fca3b
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-04-24 23:58:20 -0400

    playing around with user form did not break anything

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -40,35 +40,36 @@ class App extends React.Component {
         <div>
           <Route path='/' component={Nav} />
           <div className="container">
-            <Route path='/users/:id' render={({ match, history }) => (
-              <UserAccount history={history } id={match.params.id} />
-            )} />
-          <Switch>
-            <Route exact path='/' component={Home} />
+            <div id="body-elements">
+              <Route path='/users/:id' render={({ match, history }) => (
+                <UserAccount history={ history } id={match.params.id} />
+              )} />
+              <Switch>
+                <Route exact path='/' component={Home} />
 
-            {/* CATEGORY ROUTES */}
-            <Route exact path='/categories' component={Categories} />
-            <Route exact path='/categories/:id' component={CategoryInfo} />
-            <Route exact path='/products' component={Products} />
+                {/* CATEGORY ROUTES */}
+                <Route exact path='/categories' component={Categories} />
+                <Route exact path='/categories/:id' component={CategoryInfo} />
+                <Route exact path='/products' component={Products} />
 
-            {/* PRODUCT ROUTES */}
-            <Route exact path='/products/:id' component={ProductInfo} />
+                {/* PRODUCT ROUTES */}
+                <Route exact path='/products/:id' component={ProductInfo} />
 
-            {/* USER ROUTES */}
-            <Route exact path='/users' component={Users} />
-            <Route exact path='/users/:id/cart' component={ ActiveOrder } />
-            <Route exact path='/users/:id/orders' component={ PastOrders } />
-            <Route exact path='/users/:id/addresses' render={({ match }) => (
-              <Addresses id={ match.params.id }/>
-            )}  />
-            <Route exact path='/users/:id/edit' component={ UserForm } />
+                {/* USER ROUTES */}
+                <Route exact path='/users' component={Users} />
+                <Route exact path='/users/:id/cart' component={ ActiveOrder } />
+                <Route exact path='/users/:id/orders' component={ PastOrders } />
+                <Route exact path='/users/:id/addresses' render={({ match }) => (
+                  <Addresses id={ match.params.id }/>
+                )}  />
+                <Route exact path='/users/:id/edit' component={ UserForm } />
 
-            {/* AUTH ROUTES */}
-            <Route exact path='/login' component={LoginForm} />
-            <Route exact path='/signup' component={LoginForm} />
-
-          </Switch>
-          <Route component={Footer} />
+                {/* AUTH ROUTES */}
+                <Route exact path='/login' component={LoginForm} />
+                <Route exact path='/signup' component={LoginForm} />
+              </Switch>
+            </div>
+            <Route component={Footer} />
           </div>
         </div>
       </Router>
