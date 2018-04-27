af8ae02f34da556a4b1adb1606548116716f7654
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-04-27 07:54:33 -0400

    fixed product form

--- client/src/components/Review/Reviews.js
+++ client/src/components/Review/Reviews.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Reviews = (props) => {
   const { product, products, productReviews, userReviews, users, page } = props;
-
-  if(!product) {
-    return null;
-  }
-
   if(page === 'product') {
+    if (!product) return null
     return (
       <div>
-        <h3>{product.name}: Reviews</h3>
+        <Link to={`/products/${product.id}`}><h3>{product.name}: Reviews</h3></Link>
         {
           productReviews.map(review => {
             const user = users.find(user => user.id === review.userId);
@@ -36,7 +33,7 @@ const Reviews = (props) => {
             const product = products.find(product => product.id === review.productId);
             return (
               <div key={review.id} style={{ backgroundColor: 'lightgrey' }}>
-                <h4>{product.name}: {review.rating} Stars</h4>
+                <Link to={`/products/${product.id}/reviews`}><h4>{product.name}: {review.rating} Stars</h4></Link>
                 <p>{review.description}</p>
               </div>
             )
@@ -50,7 +47,7 @@ const Reviews = (props) => {
 }
 
 const mapState = ({ products, reviews, users }, { id, page }) => {
-  const product = products.find(product => product.id === id);
+  const product = products && products.find(product => product.id === id)
   const productReviews = reviews.filter(review => review.productId === id);
   const userReviews = reviews.filter(review => review.userId === id);
   return {
