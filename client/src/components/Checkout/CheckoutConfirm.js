2419f49bf32a901788735aeb5165c22756b99350
Jeremy Grubard <jgrubard@gmail.com>
2018-04-29 13:06:37 -0400

    fixed checkout items

--- client/src/components/Checkout/CheckoutConfirm.js
+++ client/src/components/Checkout/CheckoutConfirm.js
@@ -2,27 +2,37 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Addresses from '../Address/Addresses';
+// import ActiveOrder from '../Order/ActiveOrder';
 import ActiveOrder from '../Order/ActiveOrder';
 import PaymentForm from './PaymentForm';
 
 
 // Need a way to select address to use
-// Need to get rid of button "checkout" 
+// Need to get rid of button "checkout"
 
-const CheckoutConfirm = ({user}) => {
+const CheckoutConfirm = ({ user }) => {
   console.log('####user##', user)
+
+  if(!user.id) return null
+
   return (
     <div>
     <ActiveOrder />
-    <Addresses/>
+    <Addresses id={user.id} page='checkout' />
     <PaymentForm />
     <Link to={`/users/${user.id}/cart/checkout/thankyou`}><button className="btn btn-success">Submit Payment</button></Link>
     </div>
   );
 }
 
-const mapState = ({user}) => {
-  return { user }
+const mapState = ({ user }) => {
+  // const userAddresses = addresses.filter(address => address.userId === user.id)
+  // console.log(user)
+
+  return {
+    user,
+    // addresses: userAddresses
+  }
 };
 
 export default connect(mapState)(CheckoutConfirm);
