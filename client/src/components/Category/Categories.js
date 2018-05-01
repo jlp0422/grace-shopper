d428627d4ce534f580e60dcedd1034d6506aa7cc
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-05-01 19:48:06 -0400

    search works on category page

--- client/src/components/Category/Categories.js
+++ client/src/components/Category/Categories.js
@@ -4,29 +4,56 @@ import { Link } from 'react-router-dom';
 import CategoryCard from './CategoryCard';
 import CategoryForm from './CategoryForm';
 
-const Categories = ({ categories, loggedIn, isAdmin }) => {
-  return (
-    <div>
-      <h2>Categories</h2>
-      { loggedIn && isAdmin ? (
-        <div>
-          <Link to='/categories/create'>
-            <button className="btn btn-primary">Create new category</button>
-          </Link>
-        </div>
-      ) : null }
-      <ul className='list-group'>
-        {
-          categories.map(category => (
-            <li key={category.id} className='list-group-item'>
-              <CategoryCard category={category} />
-            </li>
-          ))
-        }
-      </ul>
-    </div>
-  );
-};
+class Categories extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: ''
+    }
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(ev) {
+    this.setState({ name: ev.target.value})
+  }
+
+  render() {
+    const { categories, loggedIn, isAdmin } = this.props
+    const { onChange } = this
+    const { name } = this.state
+    const matchingCategories = categories.reduce((memo, category) => {
+      if (category.name.toLowerCase().match(name.toLowerCase())) {
+        return memo.concat(category)
+      }
+      return memo
+    }, [])
+    return (
+      <div>
+        <h2>Categories</h2>
+        { loggedIn && isAdmin ? (
+          <div>
+            <Link to='/categories/create'>
+              <button className="btn btn-primary">Create new category</button>
+            </Link>
+            <div>
+              <input value={ name } onChange={ onChange } className="form-control" />
+            </div>
+          </div>
+        ) : null }
+        <ul className='list-group'>
+          {
+            matchingCategories.map(category => (
+              <li key={category.id} className='list-group-item'>
+                <CategoryCard category={category} />
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
 
 const mapState = ({ categories, user }) => {
   const loggedIn = !!user.id;
--- client/src/components/Product/Products.js
+++ client/src/components/Product/Products.js
@@ -8,26 +8,25 @@ class Products extends React.Component {
   constructor() {
     super()
     this.state = {
-      search: ''
+      name: ''
     }
     this.onChange = this.onChange.bind(this)
   }
 
   onChange(ev) {
-    this.setState({ search: ev.target.value })
+    this.setState({ name: ev.target.value })
   }
 
   render() {
     const { products, isAdmin, loggedIn } = this.props
     const { onChange } = this
-    const { search } = this.state
+    const { name } = this.state
     const matchingProducts = products.reduce((memo, product) => {
-      if (product.name.toLowerCase().match(search.toLowerCase())) {
+      if (product.name.toLowerCase().match(name.toLowerCase())) {
         return memo.concat(product)
       }
       return memo
     }, [])
-    console.log(matchingProducts)
     return (
       <div>
         <h2>Products</h2>
@@ -37,7 +36,7 @@ class Products extends React.Component {
               <button className="btn btn-primary">Create new product</button>
             </Link>
             <div>
-              <input value={ search } onChange={ onChange } className="form-control" />
+              <input placeholder={'Search for a product'} value={ name } onChange={ onChange } className="form-control" />
             </div>
           </div>
         ) : null }
