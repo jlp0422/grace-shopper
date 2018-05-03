df03ce94d8d0f8073b0173ed03451550827e2690
Jeremy Grubard <jgrubard@gmail.com>
2018-05-03 19:04:27 -0400

    some minor things

--- client/src/components/Checkout/ThankYou.js
+++ client/src/components/Checkout/ThankYou.js
@@ -5,22 +5,21 @@ import UserNav from '../User/UserNav';
 const ThankYou = ({ user, order }) => {
   return (
     <div>
-      <UserNav user={ user } />
+      <UserNav user={ user, orderId } />
       <div className='jumbotron'>
         <h1>J²A Widgets</h1>
         <br />
         <h3>Thank you for your order!</h3>
         <br />
-        <h4>You will receive an email shortly with details about Order#{order.id}</h4>
+        <h4>You will receive an email shortly with details about Order#{orderId}</h4>
       </div>
     </div>
   );
 }
 
 
-const mapState = ({ user, orders }) => {
-  const order = orders.find(order => order.userId === user.id)
-  return { user, order }
+const mapState = ({ user }, { orderId }) => {
+  return { user, orderId }
 }
 
 export default connect(mapState)(ThankYou);
