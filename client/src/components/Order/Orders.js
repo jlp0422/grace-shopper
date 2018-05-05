ec4033d47cb90c3772e474b10eb0a2add4b6379d
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-05-05 18:42:40 -0400

    checkbox filtering on orders works kind of

--- client/src/components/Order/Orders.js
+++ client/src/components/Order/Orders.js
@@ -2,28 +2,71 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Orders = ({ orders, user }) => {
-  const ordersToShow = user ? orders.filter(order => order.userId === user.id) : orders
-  return (
-    <div>
-      <h2>{ user ? (
-        `Order history for ${user.firstName} ${user.lastName}`
-      ) : (
-        'Complete order history'
-      )}</h2>
-      <ul className="list-group">
+/* THINGS TO ADD:
+- PAGINATION
+- SEARCH
+- FILTER FOR ORDER STATUS
+*/
+
+class Orders extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      startIndex: 0,
+      endIndex: 20,
+      statusView: 'all'
+    }
+    this.onChange = this.onChange.bind(this)
+    this.onCheck = this.onCheck.bind(this)
+    // this.onPrevPage = this.onPrevPage.bind(this)
+    // this.onNextPage = this.onNextPage.bind(this)
+  }
+
+  onChange(ev) {
+    this.setState({ name: ev.target.value, startIndex: 0, endIndex: 20 })
+  }
+
+  onCheck(ev) {
+    const { value } = ev.target
+    this.setState({ statusView: value })
+  }
+
+  render() {
+    const { orders, user } = this.props
+    const { onChange, onCheck } = this
+    const { statusView } = this.state
+    console.log(statusView)
+    const statuses = [ 'cart', 'processed', 'shipped', 'complete', 'all' ]
+    const allOrders = user ? orders.filter(order => order.userId === user.id) : orders
+    const ordersToShow = allOrders.filter(order => statusView === 'all' ? order : order.status === statusView)
+
+    return (
+      <div>
+        <h2>Order history { user ? `for ${user.firstName} ${user.lastName}` : null }</h2>
         {
-          ordersToShow.map(order => (
-            <div key={ order.id }>
-            <Link to={`/admin/orders/${order.id}`}>
-              <li className="list-group-item">Order #{order.id}</li>
-            </Link>
+          statuses.map(status => (
+            <div key={ status } className="form-check form-check-inline">
+              <input className="form-check-input" onChange={onCheck} type="radio" value={status} />
+              <label className="form-check-label">{status}</label>
             </div>
           ))
         }
-      </ul>
-    </div>
-  )
+        <ul className="list-group">
+          {
+            ordersToShow.map(order => (
+              <div key={ order.id }>
+                <li className="list-group-item">
+                  <Link to={`/admin/orders/${order.id}`}>Order #{order.id}</Link>
+                  <br/>Status: {order.status}
+                </li>
+              </div>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
 }
 
 const mapState = ({ orders, users }, { id }) => {
