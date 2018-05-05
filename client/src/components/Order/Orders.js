6111dc748cc0c780634bacd338afd33392b25e5c
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-05-05 18:51:27 -0400

    working on more func for orders admin page

--- client/src/components/Order/Orders.js
+++ client/src/components/Order/Orders.js
@@ -19,8 +19,8 @@ class Orders extends React.Component {
     }
     this.onChange = this.onChange.bind(this)
     this.onCheck = this.onCheck.bind(this)
-    // this.onPrevPage = this.onPrevPage.bind(this)
-    // this.onNextPage = this.onNextPage.bind(this)
+    this.onPrevPage = this.onPrevPage.bind(this)
+    this.onNextPage = this.onNextPage.bind(this)
   }
 
   onChange(ev) {
@@ -32,15 +32,29 @@ class Orders extends React.Component {
     this.setState({ statusView: value })
   }
 
+  onPrevPage() {
+    const { startIndex, endIndex } = this.state
+    this.setState({ startIndex: startIndex - 20, endIndex: endIndex - 20 })
+  }
+
+  onNextPage() {
+    const { startIndex, endIndex } = this.state
+    this.setState({ startIndex: startIndex + 20, endIndex: endIndex + 20 })
+  }
+
   render() {
     const { orders, user } = this.props
-    const { onChange, onCheck } = this
-    const { statusView } = this.state
-    console.log(statusView)
+    const { onChange, onCheck, onNextPage, onPrevPage } = this
+    const { statusView, endIndex, startIndex } = this.state
     const statuses = [ 'cart', 'processed', 'shipped', 'complete', 'all' ]
     const allOrders = user ? orders.filter(order => order.userId === user.id) : orders
-    const ordersToShow = allOrders.filter(order => statusView === 'all' ? order : order.status === statusView)
-
+    const ordersForStatus = allOrders.filter(order => statusView === 'all' ? order : order.status === statusView)
+    const ordersToShow = ordersForStatus.reduce((memo, order, index) => {
+      if (index < endIndex && index >= startIndex) memo.push(order)
+      return memo
+    }, [])
+    const currentPage = endIndex / 20
+    const lastPage = Math.ceil(ordersForStatus.length / 20)
     return (
       <div>
         <h2>Order history { user ? `for ${user.firstName} ${user.lastName}` : null }</h2>
@@ -64,6 +78,11 @@ class Orders extends React.Component {
             ))
           }
         </ul>
+        <div className="product-buttons">
+          <button disabled={ startIndex < 20 } className="btn btn-outline-info prev-btn" onClick={ onPrevPage }>Previous</button>
+          <button disabled className="btn btn-info">Page { currentPage } / { lastPage }</button>
+          <button disabled={ endIndex >= ordersForStatus.length } className="btn btn-outline-info next-btn" onClick={ onNextPage }>Next</button>
+        </div>
       </div>
     )
   }
