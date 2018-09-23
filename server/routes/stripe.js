40f574dabf72d95ca0f0c7cac0d5b5e0e3ca0e28
Jeremy Grubard <jgrubard@gmail.com>
2018-09-23 16:34:07 -0400

    commit

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const app = require('express').Router();
 module.exports = app;
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 
-const stripe = require('stripe')('sk_test_i1WQiMWphkAwtflNJjhTFNr4');
+const stripe = require('stripe')(STRIPE_SECRET_KEY);
 
 app.post('/', (req, res, next) => {
   stripe.charges.create(req.body)
