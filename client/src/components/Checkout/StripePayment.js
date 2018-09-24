4bef497ac2fe25db8f15a22806d580e49ed20294
Jeremy Grubard <jgrubard@gmail.com>
2018-09-24 17:05:33 -0400

    stripe working

--- client/src/components/Checkout/StripePayment.js
+++ client/src/components/Checkout/StripePayment.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
-// console.log(StripeCheckout)
-
-const onToken = (amount, email, orderId) => {
+const onToken = (amount, email, orderId, onSave) => {
   return (token) => {
     axios.post('/api/stripe', {
       source: token.id,
@@ -14,38 +12,22 @@ const onToken = (amount, email, orderId) => {
       description: `Order# ${orderId}`
     })
     .then(res => res.data)
-    .then(() => {
-      // console.log('before', tokenId);
-      window.localStorage.setItem('purchase-made', 'true');
-      // const token = window.localStorage.getItem('stripeToken');
-      // console.log('local storage:', token);
+    .then(token => {
+      if(token) onSave();
     })
   }
 }
-// window.localStorage.removeItem('purchase-made');
-const onClose = (onSave) => {
-  // const tokenId = window.localStorage.getItem('stripeToken');
-  if(!!window.localStorage.getItem('purchase-made')) {
-    console.log('purchase made')
-    // console.log('onClose:', 'purchase made?:', !!tokenId);
-    window.localStorage.removeItem('purchase-made');
-    onSave();
-  } else {
-    console.log('purchase not made')
-  }
-}
 
-const StripePayment = ({ amount, name, email, orderId, onSave, checkErrors, shippingId, billingId }) => {
+const StripePayment = ({ amount, name, email, orderId, onSave, shippingId, billingId }) => {
   return (
     <div>
     <StripeCheckout
       name={name}
       stripeKey='pk_test_L5BNU52HtQupz1A0XX4pzElV'
-      token={onToken(amount, email, orderId)}
+      token={onToken(amount, email, orderId, onSave)}
       amount={amount * 100}
       email={email}
       disabled={!shippingId || !billingId}
-      closed={() => onClose(onSave)}
     />
     {
       !shippingId || !billingId ? (
@@ -53,7 +35,6 @@ const StripePayment = ({ amount, name, email, orderId, onSave, checkErrors, ship
       ) : null
     }
     </div>
-
   );
 }
 
